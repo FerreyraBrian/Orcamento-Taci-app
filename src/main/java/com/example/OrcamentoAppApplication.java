6966e1d7e34819cfb426ab807c6ad598b0d6c8b0
FerreyraBrian <ferreyrabrian@outlook.es>
2025-08-07 23:15:36 -0300

    changes

---
+++ src/main/java/com/example/OrcamentoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrcamentoAppApplication.class, args);
+    }
+}
+
+// To run the application, use the command: mvn spring-boot:run
